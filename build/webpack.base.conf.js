42cd3d45b0e2d9493d61a337eada69a2a2247353
lynchuh <lynchuh@gmail.com>
2019-04-21 10:44:00 +0800

    添加路由配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,32 +1,34 @@
 const path = require('path')
 
-
-module.exports ={
+module.exports = {
   output: {
-    path: path.join(__dirname,'../dist'),
-    publicPath: "/public/",
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /.(jsx|js)$/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: [
-          path.join(__dirname,'../node_modules')
+          path.join(__dirname, '../node_modules')
         ]
       },
       {
         test: /.jsx$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /.js$/,
         loader: 'babel-loader',
         exclude: [
-          path.join(__dirname,'../node_modules')
+          path.join(__dirname, '../node_modules')
         ]
       }
     ]
-  },
+  }
 }
